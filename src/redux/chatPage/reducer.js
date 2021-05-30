74758de8e8e70e05ff476feefbd3b88d2b795eeb
mansoureh mahzadeh <mansoureh.1998@gmail.com>
2021-05-30 21:03:12 +0430

    add sample msg

--- src/redux/chatPage/reducer.js
+++ src/redux/chatPage/reducer.js
@@ -1,7 +1,63 @@
 import { PROFILE_DETAILS } from './types'
 
 const initialState = {
-    item: [],
+    data: [
+        {
+            id: 1,
+            user: 'me',
+            msg: 'hi',
+        },
+        {
+            id: 2,
+            user: 'sampleUser',
+            msg: 'hi !'
+        },
+        {
+            id: 3,
+            user: 'me',
+            msg: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, molestiae!'
+        },
+        {
+            id: 4,
+            user: 'sampleUser',
+            msg: 'Lorem ipsum dolor sit amet consectetur'
+        },
+        {
+            id: 5,
+            user: 'me',
+            msg: ':)))'
+        },
+        {
+            id: 6,
+            user: 'me',
+            msg: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore'
+        },
+        {
+            id: 7,
+            user: 'sampleUser',
+            msg: 'Lorem ipsum dolor sit amet'
+        },
+        {
+            id: 8,
+            user: 'sampleUser',
+            msg: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, molestiae!'
+        },
+        {
+            id: 9,
+            user: 'sampleUser',
+            msg: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+        },
+        {
+            id: 10,
+            user: 'me',
+            msg: 'Lorem ipsum dolor.'
+        },
+        {
+            id: 11,
+            user: 'sampleUser',
+            msg: 'Lorem ipsum dolor sit.'
+        }
+    ],
     open: false
 
 }
