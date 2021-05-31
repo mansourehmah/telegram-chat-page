8c93a005d051a7fbc94c1b8e580dbe0d4c37f484
mansoureh mahzadeh <mansoureh.1998@gmail.com>
2021-05-31 17:15:18 +0430

    fix style bug

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -8,13 +8,11 @@ function ChatPage() {
     const openDetails = useSelector(state => state.chatPage.open)
 
     return (
-        <div className='chatPageWrapper col-12 col-md-9 p-0'>
-            <div className="chatPage col-12 p-0 d-flex flex-column">
-                <Header openDetails={openDetails} />
-                <div className={openDetails ? "transformToBottom bottomWrapper d-flex flex-column" : "bottomWrapper d-flex flex-column"} >
-                    <MessageWrapper />
-                    <InputMessage />
-                </div>
+        <div className="chatPage col-12 col-md-9 p-0 d-flex flex-column">
+            <Header openDetails={openDetails} />
+            <div className={openDetails ? "transformToBottom bottomWrapper d-flex flex-column" : "bottomWrapper d-flex flex-column"} >
+                <MessageWrapper />
+                <InputMessage />
             </div>
         </div>
     );
